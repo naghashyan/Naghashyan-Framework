fe3cfa999ae472e6478490c1b06fb000f2a34b08
Levon <levon@naghashyan.com>
2015-10-29 20:25:55 +0400

    fixed demo welcome pages

--- conf/constants.php
+++ conf/constants.php
@@ -17,6 +17,8 @@
 //defaining project version
 NGS()->define("VERSION", "1.0.0");
 
+NGS()->define("SESSION_MANAGER", 'demo\managers\SessionManager');
+
 //define error show status
 if (NGS()->getDefinedValue("ENVIRONMENT") != "production") {
 	error_reporting(E_ALL);
--- templates/main/home.tpl
+++ templates/main/home.tpl
@@ -1,4 +1,6 @@
 <div class="welcome">
   <h1>nested home load</h1>
+  {if isset($ns.demoDto)}
   <h2>Hello {$ns.demoDto->getName()}</h2>
+  {/if}
 </div>
